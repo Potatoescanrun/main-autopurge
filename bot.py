a3d8ee1e15c05ac31e846b4993daf6ddfb3195ee
CoolDragon675 <s201501052@gmail.com>
2026-03-27 23:51:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,21 +3,12 @@ import os
 
 def run_purge():
     site = pywikibot.Site()
-    bot_password = os.environ.get('PYWIKIBOT_PASSWORD')
     
-    if not bot_password:
-        print("Error: PYWIKIBOT_PASSWORD not found.")
-        return
+    password = os.environ.get('PYWIKIBOT_PASSWORD')
+    site.login(password=password)
 
-    site.login(password=bot_password)
-
-    page_title = "Main Page" 
-    page = pywikibot.Page(site, page_title)
-
-    if page.purge():
-        print(f"Successfully purged '{page_title}'.")
-    else:
-        print(f"Failed to purge '{page_title}'.")
+    page = pywikibot.Page(site, "Main Page")
+    page.purge()
 
 if __name__ == "__main__":
     run_purge()
